6a14433edcd1df26792d49356d2d0543e7a65145
Leonardo <leosouzamonteiro123@gmail.com>
2017-06-20 20:01:23 +0000

    inicio do desenvolvimento menus mobile topo e footer

---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+  <title>Soul Team - Inicio</title>
+  <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/css.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+</head>
+<body>
+  <header>
+    <nav class="navbar-fixed blue-grey darken-4 white-text" role="navigation">
+      <div class="nav-wrapper container">
+        <a id="logo-container" href="#" class="brand-logo">
+          <span class="orange-st-text">S</span>oul Te<span class="orange-st-text">a</span>m
+        </a>
+        <ul class="right hide-on-med-and-down">
+          <li>
+            <a href="#">Sobre</a>
+          </li>
+          <li>
+            <a href="#">Serviços</a>
+          </li>
+          <li>
+            <a href="#">Portifolio</a>
+          </li>
+          <li>
+            <a href="#">Contato</a>
+          </li>
+        </ul>
+      
+        <ul id="nav-mobile" class="side-nav">
+          <li>
+            <a href="#">Sobre</a>
+          </li>
+          <li>
+            <a href="#">Serviços</a>
+          </li>
+          <li>
+            <a href="#">Portifolio</a>
+          </li>
+          <li>
+            <a href="#">Contato</a>
+          </li>
+        </ul>
+        <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+      </div>
+    </nav>
+  </header>
+  <main>
+    <section class="inicio scrollspy light-blue-st" id="inicio">
+      <h1 class="center white-text">Soluções que sua empresa precisa!</h1>
+    </section>
+    <section class="sobre scrollspy blue-grey lighten-3" id="sobre">
+      <h1 class="center blue-text text-darken-4">Quem somos</h1>
+    </section>
+    <section class="servicos scrollspy orange-st" id="servicos">
+      <h1 class="center white-text">Serviços</h1>
+    </section>
+    <section class="contato scrollspy blue-st" id="contato">
+      <h1 class="center white-text">Contato</h1>
+      <form class="" id="" action="" method="post">
+        
+      </form>
+    </section>
+  </main>
+  <footer class="page-footer blue-grey darken-4 white-text">
+    <div class="container">
+      <div class="row">
+        <h2>
+          <span class="orange-st-text">S</span>oul Te<span class="orange-st-text">a</span>m
+        </h2>
+      </div>
+      <div class="row">
+        <div class="col l5 s12">
+          <h5 class="">Mapa do site</h5>
+          <ul>
+            <li>
+              <a class="white-text" href="#">Sobre</a>
+            </li>
+            <li>
+              <a class="white-text" href="#">Serviços</a>
+            </li>
+            <li>
+              <a class="white-text" href="#">Portifolio</a>
+            </li>
+            <li>
+              <a class="white-text" href="#">Contato</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col l5 s12">
+          <h5 class="">Institucional</h5>
+          <ul>
+            <li>
+              <a class="white-text" href="#">FAQ</a>
+            </li>
+            <li>
+              <a class="white-text" href="#">Missão e valores</a>
+            </li>
+            <li>
+              <a class="white-text" href="#">Quem somos</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col l2 s12">
+          <h5 class="">Rede Sociais</h5>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+        <a class="white-text" href="http://materializecss.com">Equipe de desenvolvimento</a>
+      </div>
+    </div>
+  </footer>
+</body>
+</html>
